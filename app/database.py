7c200c4016472aff7c8a0ae0f8b055a6f8744514
goim-ved <vedantgoim1@gmail.com>
2026-09-09 05:57:06 +0530

    spelling

--- app/database.py
+++ app/database.py
@@ -9,7 +9,7 @@ if settings.DATABASE_URL.startswith("sqlite"):
 engine = create_engine(
     settings.DATABASE_URL,
     connect_args=connect_args,
-    pool_are_ping=True
+    pool_pre_ping=True
 )
 
 SessionLocal = sessionmaker(
